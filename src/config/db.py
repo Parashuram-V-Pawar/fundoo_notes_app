7e961497efb7ec068677e7b389e85caa67059c37
Parashuram-V-Pawar <140330600+Parashuram-V-Pawar@users.noreply.github.com>
2026-03-24 20:50:14 +0530

    Revert "Feature/users"

--- src/config/db.py
+++ src/config/db.py
@@ -2,18 +2,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-from urllib.parse import quote_plus
 
 load_dotenv()
 
 username = os.getenv('USERNAME')
-password = quote_plus(os.getenv('PASSWORD'))
-host = os.getenv('DB_HOST')
+password = os.getenv('PASSWORD')
 port = os.getenv('PORT_NUMBER')
 database = os.getenv('DATABASE')
 
 DATABASE_URL = (
-    f"mssql+pyodbc://{username}:{password}@{host},{port}/"
+    f"mssql+pyodbc://{username}:{password}@localhost,{port}/"
     f"{database}?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes")
 
 engine = create_engine(DATABASE_URL)
