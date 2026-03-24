0fb3e5701db6fa0affcbfc1db306d2ca4595f0cf
Parashuram <parashurampawar56@gmail.com>
2026-03-24 22:34:49 +0530

    modify program: [parashuram] updated main program

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from src.config.db import Base, engine
-from src.models import users, notes
+from src.config.logger_middleware import log_requests
+from src.models import notes, users
+from src.routes import notes_routes  
 
 app = FastAPI()
+app.middleware("http")(log_requests)
 
 # Create tables
 Base.metadata.create_all(bind=engine)
-
-@app.get("/")
-def hello():
-    return '{message:"Hello world"}'
+app.include_router(notes_routes.router)
