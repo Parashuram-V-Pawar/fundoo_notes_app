5bc824b73ca822158432c5ff0c26b249431c828c
Parashuram <parashurampawar56@gmail.com>
2026-03-24 20:55:53 +0530

    added program: [parashuram] Added program to define notes table structure

---
+++ src/models/notes.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, ForeignKey, String
+
+from src.config.db import Base
+
+class Note(Base):
+    __tablename__ = 'notes'
+    
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    description = Column(String, nullable=False)
+    
